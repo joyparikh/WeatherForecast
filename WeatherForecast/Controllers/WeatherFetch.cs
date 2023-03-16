f3570f79a9ad5a5d67ed13a9d90ddfae59d758f0
Joy Parikh <jvp2118@columbia.edu>
2023-03-16 15:04:25 -0700

    adaptive bg

--- WeatherForecast/Controllers/WeatherFetch.cs
+++ WeatherForecast/Controllers/WeatherFetch.cs
@@ -15,10 +15,31 @@ namespace WeatherForecast.Controllers
         {
             string APIkey = "0cd9db1aa08c4c14949230244231303";
             string uri = string.Format("https://api.weatherapi.com/v1/forecast.json?q={0}&days=14&key={1}", q, APIkey);
+            string sun = "sun";
+            string snow = "snow";
+            string cloud = "cloud";
+            string rain = "rain";
             using (WebClient web = new WebClient())
             {
                 var json = web.DownloadString(uri);
                 Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
+
+                if (myDeserializedClass.current.condition.text.ToLower().Contains(sun))
+                {
+                    ViewData["color"] = "#e3dd36"; 
+                }
+                if (myDeserializedClass.current.condition.text.ToLower().Contains(snow))
+                {
+                    ViewData["color"] = "#FFFFFF";
+                }
+                if (myDeserializedClass.current.condition.text.ToLower().Contains(rain))
+                {
+                    ViewData["color"] = "#528079";
+                }
+                if (myDeserializedClass.current.condition.text.ToLower().Contains(cloud) || myDeserializedClass.current.condition.text.ToLower().Contains("mist"))
+                {
+                    ViewData["color"] = "#b1b5b5";
+                }
                 ViewData["temp"] = myDeserializedClass.forecast.forecastday;
                 ViewData["curr"] = myDeserializedClass.current;
                 ViewData["loc"] = myDeserializedClass.location;
