d924f280ff4f632a8be6b518c1c1c719d6b92034
Joy Parikh <jvp2118@columbia.edu>
2023-03-16 13:03:43 -0700

    code cleanup

--- WeatherForecast/Controllers/WeatherFetch.cs
+++ WeatherForecast/Controllers/WeatherFetch.cs
@@ -15,17 +15,10 @@ namespace WeatherForecast.Controllers
         {
             string APIkey = "0cd9db1aa08c4c14949230244231303";
             string uri = string.Format("https://api.weatherapi.com/v1/forecast.json?q={0}&days=14&key={1}", q, APIkey);
-            Debug.WriteLine("fetching data from ", uri);
             using (WebClient web = new WebClient())
             {
                 var json = web.DownloadString(uri);
                 Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(json);
-                //List<string> condList = new List<string>();
-                //foreach (var item in myDeserializedClass.forecast.forecastday)
-                //{
-                //    condList.Add(item.day.condition.text);
-                //}
-                //ViewData["temp"] = condList;
                 ViewData["temp"] = myDeserializedClass.forecast.forecastday;
                 ViewData["curr"] = myDeserializedClass.current;
                 ViewData["loc"] = myDeserializedClass.location;
